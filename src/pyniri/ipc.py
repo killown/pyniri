bd0ce45da2df78d4d5524524d20294b4c92d23f6
Thiago <killown.matrix@gmail.com>
2026-02-17 20:41:56 -0300

    add LiCENSE

--- src/pyniri/ipc.py
+++ src/pyniri/ipc.py
@@ -1,3 +1,11 @@
+# pyniri: Pythonic IPC bindings for the niri scroll-tiling compositor
+# Copyright (C) 2026 Thiago <killown.matrix@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import socket
 import json
 import os
